e8f7666eae2ba7f9482f77168cf4248bf024a91a
mrstnj <>
2025-02-09 20:23:55 +0900

    add: OAuthトークン取得処理

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class OauthController < ApplicationController
   def authorize
     request_url = 'http://unifa-recruit-my-tweet-app.ap-northeast-1.elasticbeanstalk.com/oauth/authorize'
@@ -12,5 +14,23 @@ class OauthController < ApplicationController
   end
 
   def callback
+    request_url = 'http://unifa-recruit-my-tweet-app.ap-northeast-1.elasticbeanstalk.com/oauth/token'
+    query_params = {
+      client_id: Rails.application.credentials.oauth[:client_id],
+      client_secret: Rails.application.credentials.oauth[:client_secret],
+      redirect_uri: 'http://localhost:3000/oauth/callback',
+      grant_type: 'authorization_code',
+      code: params[:code],
+    }
+
+    uri = URI.parse(request_url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.set_form_data(query_params)
+    response = http.request(request)
+    session[:oauth_access_token] = JSON.parse(response.body)['access_token']
+
+    redirect_to user_images_path
   end
 end
+
