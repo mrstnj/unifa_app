1df6f4c231ba2f69800d4f6c2d1b9e980c7dc625
mrstnj <>
2025-02-09 15:05:00 +0900

    add: image_processing

--- app/models/user_image.rb
+++ app/models/user_image.rb
@@ -2,6 +2,6 @@ class UserImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  validates :title, presence: { message: "タイトルを入力してください" }, length: { maximum: 2, message: "タイトルは30文字以内で入力してください" }
+  validates :title, presence: { message: "タイトルを入力してください" }, length: { maximum: 30, message: "タイトルは30文字以内で入力してください" }
   validates :image, presence: { message: "画像ファイルを入力してください" }
 end
