51f28c2dbc456995898fae287edd1fd00700a6f0
mrstnj <>
2025-02-11 11:26:31 +0900

    add: 拡張子・サイズ制限

--- app/models/user_image.rb
+++ app/models/user_image.rb
@@ -4,4 +4,20 @@ class UserImage < ApplicationRecord
 
   validates :title, presence: { message: "タイトルを入力してください" }, length: { maximum: 30, message: "タイトルは30文字以内で入力してください" }
   validates :image, presence: { message: "画像ファイルを入力してください" }
+  validate :acceptable_image
+
+  private
+
+  def acceptable_image
+    return unless image.attached?
+
+    unless image.byte_size <= 5.megabyte
+      errors.add(:image, message: '画像ファイルのサイズは5MB以下にしてください')
+    end
+
+    acceptable_types = ['image/jpeg', 'image/jpg', 'image/png']
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, message: '画像ファイルはJPG、JPEG、PNG形式のみ対応しています')
+    end
+  end
 end
